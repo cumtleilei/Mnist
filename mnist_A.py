1172dd557f16f47db18fe4333336064e49151887
cumtleilei <plpl9595@163.com>
2018-12-14 21:27:48 +0800

    initial

---
+++ mnist_A.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("data/", one_hot=True)
+
+batch_size = 50
+
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+
+w = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+pred = tf.nn.softmax(tf.matmul(x, w) + b)
+
+cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), 1))
+
+optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(25):
+        avg_cost = 0.
+
+        total_batch = int(mnist.train.num_examples/batch_size)
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            sess.run(optimizer, {x: batch_xs, y: batch_ys})
+
+            avg_cost += sess.run(cost, {x: batch_xs, y: batch_ys}) / total_batch
+
+        if (epoch+1) % 5 == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+
+    print("Running complete...")
+
+    correct = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    print("Correction:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
